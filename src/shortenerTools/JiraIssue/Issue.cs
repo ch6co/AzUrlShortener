04eb5436c6926b9e364d3d19fb9684ed50beb561
Bryon Williams <bryon.williams@live.com>
2020-11-30 09:58:31 -0800

    add issue summary

--- src/shortenerTools/JiraIssue/Issue.cs
+++ src/shortenerTools/JiraIssue/Issue.cs
@@ -16,13 +16,15 @@ namespace shortenerTools.JiraIssue
     {
         [FunctionName("Issue")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "issue/{val1}/{val2}")] HttpRequest req,
-            string val1, string val2,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "issue/{val1}/{val2}/{val3?}")] HttpRequest req,
+            string val1, string val2, string val3,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var redirectUrl = $"https://chromehearts.atlassian.net/servicedesk/customer/portal/7/group/28/create/144?customfield_10638={val1}&customfield_10639={val2}";
+            var redirectUrl = val3 == null 
+                ? $"https://chromehearts.atlassian.net/servicedesk/customer/portal/7/group/28/create/144?customfield_10638={val1}&customfield_10639={val2}"
+                : $"https://chromehearts.atlassian.net/servicedesk/customer/portal/7/group/28/create/144?customfield_10638={val1}&customfield_10639={val2}&summary=Issue%20with%20{val3}";
             return new RedirectResult(redirectUrl);
         }
     }
